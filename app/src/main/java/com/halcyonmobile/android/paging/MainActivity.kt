f76ba945afe2978ba042f7c37267ce036c300260
Gergely Hegedus <gergo@halcyonmobile.com>
2020-04-08 08:15:57 +0300

    initial project

---
+++ app/src/main/java/com/halcyonmobile/android/paging/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.halcyonmobile.android.paging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.halcyonmobile.android.paging.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
