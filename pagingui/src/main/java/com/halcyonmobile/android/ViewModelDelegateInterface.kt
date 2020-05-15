a1f4838c83d8b065bc86dc0302990b3e38fe0956
Bianca Dragomir <bianca.dragomir@halcyonmobile.com>
2020-05-15 10:59:46 +0300

    Explain the viewModel delegate interface and its members and methods

--- pagingui/src/main/java/com/halcyonmobile/android/ViewModelDelegateInterface.kt
+++ pagingui/src/main/java/com/halcyonmobile/android/ViewModelDelegateInterface.kt
@@ -2,6 +2,13 @@ package com.halcyonmobile.android
 
 import androidx.lifecycle.LiveData
 
+/**
+ * The viewModel class should implement this interface such that it can rely on these overriden properties for keeping track of
+ * [state] - current state of the screen, which can be one of the values from [ViewState]
+ * [data] - current data
+ * [fetch] - action for fetching the data
+ * [retry] - retry action for the cases when the fetch action fails
+ */
 interface ViewModelDelegateInterface<T> {
 
     val state: LiveData<ViewState>
