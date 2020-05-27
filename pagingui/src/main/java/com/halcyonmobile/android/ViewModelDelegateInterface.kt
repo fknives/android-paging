18dd105588bebbe2df44beb7cbc083dfc02ce8af
Bianca Dragomir <bianca.dragomir@halcyonmobile.com>
2020-05-27 11:31:21 +0300

    Fix PR comments

--- pagingui/src/main/java/com/halcyonmobile/android/ViewModelDelegateInterface.kt
+++ pagingui/src/main/java/com/halcyonmobile/android/ViewModelDelegateInterface.kt
@@ -1,9 +1,10 @@
 package com.halcyonmobile.android
 
 import androidx.lifecycle.LiveData
+import com.halcyonmobile.android.ViewModelDelegateInterface.ViewState
 
 /**
- * The viewModel class should implement this interface such that it can rely on these overriden properties for keeping track of
+ * The viewModel class should implement this interface such that it can rely on these overridden properties for keeping track of
  * [state] - current state of the screen, which can be one of the values from [ViewState]
  * [data] - current data
  * [fetch] - action for fetching the data
@@ -14,13 +15,16 @@ interface ViewModelDelegateInterface<T> {
     val state: LiveData<ViewState>
     val data: LiveData<List<T>>
 
-    fun fetch(): List<T>
-    fun retry(): List<T>
+    fun fetch()
+    fun retry()
 
     enum class ViewState {
         LOADING,
         REFRESHING,
         LOADED,
-        ERROR
+        LOADING_MORE,
+        ERROR,
+        ERROR_LOADING_MORE,
+        EMPTY
     }
 }
